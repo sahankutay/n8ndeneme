c1f2dcfc4ad31365b57da1d00edc240c004ca92a
sahankutay <146508025+sahankutay@users.noreply.github.com>
2025-03-15 16:51:51 +0300

    Added main function to test sigmoid

--- my_ai_generated_code/sigmoid.cpp
+++ my_ai_generated_code/sigmoid.cpp
@@ -4,3 +4,11 @@
 double sigmoid(double x) {
     return 1.0 / (1.0 + exp(-x));
 }
+
+int main() {
+    // Test the sigmoid function
+    double test_value = 0.0;
+    double result = sigmoid(test_value);
+    printf("Sigmoid of %f is %f\n", test_value, result);
+    return 0;
+}
